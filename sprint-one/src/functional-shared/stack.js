a130e95cf3dc7b97714c104af79a218e3b65d9f1
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-14 06:58:22 +0900

    Implement stack for functional-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = {};
+
+  instance.storage = {};
+
+  _.extend(instance, stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    var highestIndex = stackMethods.findHighestIndex(this.storage);
+    console.log(typeof highestIndex);
+    if (this.storage[highestIndex] === undefined) {
+      highestIndex = parseInt(highestIndex) - 1;
+    }
+    this.storage[parseInt(highestIndex) + 1] = value;
+  },
+  pop: function() {
+    var highestIndex = stackMethods.findHighestIndex(this.storage);
+
+    var popped = this.storage[highestIndex];
+    delete this.storage[highestIndex];
+    return popped;
+  },
+  size: function() {
+    var counter = 0;
+    _.each(this.storage, function(item) {
+      counter++;
+    });
+    return counter;
+  },
+  findHighestIndex: function(obj) {
+    return _.reduce(obj, function(highest, item, key) {
+      return key > highest ? key : highest;
+    }, 0);
+  }
+};
 
 
