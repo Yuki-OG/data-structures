5f0a5502ead2e0a335a5284b629dad020b45e6db
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-14 02:57:16 +0900

    Implement a stack using the functional pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,29 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var highestIndex = _.reduce(storage, function(highest, item, key) {
+      return key > highest ? key : highest;
+    }, 0);
+    storage[highestIndex + 1] = value;
   };
 
   someInstance.pop = function() {
+    var highestIndex = _.reduce(storage, function(highest, item, key) {
+      return key > highest ? key : highest;
+    }, 0);
+    var popped = storage[highestIndex];
+    delete storage[highestIndex];
+    return popped;
+
   };
 
   someInstance.size = function() {
+    var count = 0;
+    _.each(storage, function(item) {
+      count++;
+    });
+    return count;
+    // return _.size(storage);
   };
 
   return someInstance;
