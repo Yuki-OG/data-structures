c15ac088697ee2d9f276dd7517b7e0cb4de9b4a9
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-14 05:33:54 +0900

    Implement queue using the functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,35 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    var index = _.reduce(storage, function(highest, item, key) {
+      return key > highest ? key : highest;
+    }, 0);
+
+    if (storage[index] !== undefined) {
+      while (index > -1) {
+        storage[index + 1] = storage[index];
+        index -= 1;
+      }
+    }
+    storage[0] = value;
   };
 
   someInstance.dequeue = function() {
+    var highestIndex = _.reduce(storage, function(highest, item, key) {
+      return key > highest ? key : highest;
+    }, 0);
+    var dequeued = storage[highestIndex];
+    delete storage[highestIndex];
+    return dequeued;
   };
 
   someInstance.size = function() {
+    var count = 0;
+    _.each(storage, function(item) {
+      count++;
+    });
+    return count;
   };
 
   return someInstance;
