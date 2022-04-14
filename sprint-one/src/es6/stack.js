8ef1cce29bfb62599a3b54bc5d9b96a74f005d1d
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-15 00:24:50 +0900

    Remove console.log lines that were still present

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,31 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    var highestIndex = this.getHighestIndex(this.storage);
+    this.storage[highestIndex + 1] = value;
+  }
+
+  pop() {
+    var highestIndex = this.getHighestIndex(this.storage);
+    var popped = this.storage[highestIndex];
+    delete this.storage[highestIndex];
+    return popped;
+  }
+
+  size() {
+    var counter = 0;
+    _.each(this.storage, function(item) {
+      counter++;
+    });
+    return counter;
+  }
+
+  getHighestIndex(obj) {
+    return _.reduce(obj, function(highest, item, key) {
+      return key > highest ? parseInt(key) : highest;
+    }, 0);
+  }
 }
