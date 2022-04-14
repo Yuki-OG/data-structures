a240ab3b6324b0559cb7b22fbf356ae97622d070
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-15 00:34:49 +0900

    Implement queue using the es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,37 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    var highestIndex = this.getHighestIndex(this.storage);
+    if (this.storage[highestIndex] !== undefined) {
+      while (highestIndex > -1) {
+        this.storage[highestIndex + 1] = this.storage[highestIndex];
+        highestIndex--;
+      }
+    }
+    this.storage[0] = value;
+  }
+
+  dequeue() {
+    var highestIndex = this.getHighestIndex(this.storage);
+    var dequeued = this.storage[highestIndex];
+    delete this.storage[highestIndex];
+    return dequeued;
+  }
+
+  size() {
+    var counter = 0;
+    _.each(this.storage, function(item) {
+      counter++;
+    });
+    return counter;
+  }
+
+  getHighestIndex(obj) {
+    return _.reduce(obj, function(highest, item, key) {
+      return key > highest ? parseInt(key) : highest;
+    }, 0);
+  }
 }
