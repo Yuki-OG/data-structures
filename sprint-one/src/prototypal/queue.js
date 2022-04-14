32bbe2a75aff1d374a212049be2649ff6fadebca
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-14 20:19:43 +0900

    Implement queue using the prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,41 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+
+  instance.storage = {};
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    var highestIndex = queueMethods.getHighestIndex(this.storage);
+
+    if (this.storage[highestIndex] !== undefined) {
+      while (highestIndex > -1) {
+        this.storage[highestIndex + 1] = this.storage[highestIndex];
+        highestIndex--;
+      }
+    }
+    this.storage[0] = value;
+  },
+  dequeue: function() {
+    var highestIndex = queueMethods.getHighestIndex(this.storage);
+    var dequeued = this.storage[highestIndex];
+    delete this.storage[highestIndex];
+    return dequeued;
+  },
+  size: function() {
+    var counter = 0;
+    _.each(this.storage, function(item) {
+      counter++;
+    });
+    return counter;
+  },
+  getHighestIndex: function(obj) {
+    return _.reduce(obj, function(highest, item, key) {
+      return key > highest ? parseInt(key) : highest;
+    }, 0);
+  }
+};
 
 
