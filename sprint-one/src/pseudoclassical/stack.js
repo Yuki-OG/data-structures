00348d97c64c9c77fef0d405af4362c8a2633ba3
Yuki Ogawa <tcb.yukiogawa@gmail.com>
2022-04-14 20:42:37 +0900

    Implement stack using the pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,31 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+Stack.prototype.push = function(value) {
+  var highestIndex = getHighestIndex(this.storage);
+  this.storage[highestIndex + 1] = value;
+};
+
+Stack.prototype.pop = function() {
+  var highestIndex = getHighestIndex(this.storage);
+  var popped = this.storage[highestIndex];
+  delete this.storage[highestIndex];
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  var counter = 0;
+  _.each(this.storage, function(item) {
+    counter++;
+  });
+  return counter;
+};
+
+var getHighestIndex = function(obj) {
+  return _.reduce(obj, function(highest, item, key) {
+    return key > highest ? parseInt(key) : highest;
+  }, 0);
 };
 
 
